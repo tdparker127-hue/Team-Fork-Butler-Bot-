0592a87faf5294c0487073ebbdf088331f553c43
JonahJ27 <jobomcbean@gmail.com>
2026-05-04 18:11:13 -0400

    add new config fixes

--- Jetson/config.py
+++ Jetson/config.py
@@ -247,7 +247,7 @@ STOP_BBOX_FRAC = 0.25   # fallback: STOP if person bbox > 25% of frame area
 # ===========================================================================
 
 COLOR_CAMERA_DEVICE = "/dev/video0"    # RGB camera (AprilTags / color blob)
-DEPTH_CAMERA_DEVICE = "/dev/video7"   # Depth / secondary RGB camera
+DEPTH_CAMERA_DEVICE = "/dev/video6"   # Depth / secondary RGB camera
 
 
 # ===========================================================================
