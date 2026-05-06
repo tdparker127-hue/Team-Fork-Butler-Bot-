6a0e3280e5ab4fe3763fc08fc281a6be1a7bcea3
JonahJ27 <jobomcbean@gmail.com>
2026-05-06 10:04:33 -0400

    new lowness

--- Jetson/main/simple_autonomy_controller.py
+++ Jetson/main/simple_autonomy_controller.py
@@ -184,14 +184,14 @@ MISSIONS = {
         },
         # {"type": "turn_yaw", "yaw_deg": 0.0, "tol_deg": 2.0, "hold_s": 0.5},
         {"type": "drive_tag", "tag": 6, "stop_dist": 1.34, "lat_off": 0.0},
-        {"type": "set_arm", "lift": 1.85, "grip": 1.85},
+        {"type": "set_arm", "lift": 1.84, "grip": 1.85},
         {
             "type": "drive_tag",
             "tag": 6,
             "stop_dist": 0.72,
             "lat_off": 0.0,
         },  # limit 0.62
-        {"type": "set_arm", "lift": 2.93, "grip": 0.38},
+        {"type": "set_arm", "lift": 1.84, "grip": 0.38},
         {
             "type": "drive_arm",
             "tag": 6,
