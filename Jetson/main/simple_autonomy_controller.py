5fa5e3be484db20482b45ddfe58ea8819cf8004b
JonahJ27 <jobomcbean@gmail.com>
2026-05-06 10:29:26 -0400

    new gain

--- Jetson/main/simple_autonomy_controller.py
+++ Jetson/main/simple_autonomy_controller.py
@@ -90,7 +90,7 @@ WIN_NAME   = "Autonomy Controller  (q / Esc = quit)"
 
 # -- Autonomous control --------------------------------------------------------
 TARGET_TAG_ID  = 6      # AprilTag ID highlighted in the camera overlay
-K_FWD          = 2.0    # forward P-gain  (normalized speed / m error)
+K_FWD          = 2.2    # forward P-gain  (normalized speed / m error)
 K_LAT          = 0.3    # lateral P-gain  (normalized speed / normalized pixel error [-1,1])
 K_YAW          = 0.5    # yaw P-gain from normalized pixel error
 
